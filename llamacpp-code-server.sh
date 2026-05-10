46c8985beb8c8fa5ee2b926ea0c3b14369d59232
Donatas Mažionis <1093394+donatasm@users.noreply.github.com@gmail.com>
2026-05-10 16:28:58 +0300

    Change the model to qwen3-coder-next

--- llamacpp-code-server.sh
+++ llamacpp-code-server.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
 llama-server \
-    -hf unsloth/Qwen3-Coder-30B-A3B-Instruct-GGUF:Q4_K_M \
+    -hf unsloth/Qwen3-Coder-Next-GGUF:UD-IQ3_S \
+    -fa on \
     --host 127.0.0.1 \
     --port 8079 \
     --ctx_size 65536 \
